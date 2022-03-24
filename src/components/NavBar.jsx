ff531d4a115d311cc1086f9dcbd8cd7d3ecf5d0d
david <davoodsh000@gmail.com>
2022-03-24 16:42:20 +0430

    add active navbar-item

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,23 +1,29 @@
 import "./../styles/navbar.css";
-import { Link } from "react-router-dom";
+import { Link,useLocation} from "react-router-dom";
 import {FaCompass,FaUser,fatag, FaTag}from 'react-icons/fa'
 const NavBar = () => {
+  const location=useLocation()
+  const mathRoute=(path)=>{
+    if(path==location.pathname){
+      return true
+    }
+  }
   return (
     <footer>
       <nav className="navbar">
-        <div className="nav-item">
+        <div className={`nav-item ${mathRoute('/explore')?'active':''}`}>
             <Link to='explore' className="nav-link">
             <FaCompass />
             Explore
             </Link>
         </div>
-        <div className="nav-item">
+        <div className={`nav-item ${mathRoute('/offers')?'active':''}`}>
             <Link to='offers' className="nav-link">
             <FaTag />
             Offers
             </Link>
         </div>
-        <div className="nav-item">
+        <div className={`nav-item ${mathRoute('/profile')?'active':''}`}>
             <Link to='profile' className="nav-link">
             <FaUser />
             Profile
